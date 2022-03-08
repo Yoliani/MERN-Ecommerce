14bc623f689aaeef6c0eac72a02dcdb439619538
Edgardo Yoliani <edrayoca@guarapolabs.com>
2022-03-08 11:38:26 -0500

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product" element={<ProductScreen />}>
+              <Route path=":id" element={<ProductScreen />} />
+            </Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
