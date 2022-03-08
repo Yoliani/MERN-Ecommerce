0c36dcad1fe4b9d54d6c4d2001ea9e5966078bd2
Edgardo Yoliani <edrayoca@guarapolabs.com>
2022-03-08 13:23:57 -0500

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
-  } catch (error) {}
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline);
+    process.exit(1);
+  }
 };
+
+export default connectDB;
